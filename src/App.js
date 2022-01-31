99f55016f3ec43970e71d507a2494a23a7fbbffd
jakwrobel <jak.wrobel96@gmail.com>
2022-01-31 15:42:36 +0100

    Start creating searchbar structure and add filtering functions

--- src/App.js
+++ src/App.js
@@ -3,20 +3,50 @@ import { getData } from "./utils/getData";
 import { SearchBar } from "./components/searchbar/SearchBar";
 import { Results } from "./components/results/Results";
 const App = () => {
-  const [companies, setCompanies] = useState([]);
+  const [companies, setCompanies] = useState({
+    all: [],
+    searched: [],
+  });
 
   useEffect(
     () =>
       getData()
-        .then((res) => setCompanies(res))
+        .then((res) => {
+          setCompanies({ all: res, searched: res });
+        })
         .catch((error) => console.log(error)),
     []
   );
 
+  const createUniques = (data, propName) => {
+    return data
+      .map((el) => el[propName])
+      .filter((value, index, self) => self.indexOf(value) === index);
+  };
+
+  const checkLength = (value, element) => {
+    return value.length > 0 ? value : element;
+  };
+
+  const filterData = (country, industry) => {
+    setCompanies((state) => ({
+      ...state,
+      searched: state.all.filter(
+        (el) =>
+          el.country === checkLength(country, el.country) &&
+          el.industry === checkLength(industry, el.industry)
+      ),
+    }));
+  };
+
   return (
     <div className="App">
-      <SearchBar />
-      <Results results={companies} />
+      <SearchBar
+        allData={companies.all}
+        createUniques={createUniques}
+        filterData={filterData}
+      />
+      <Results results={companies.searched} />
     </div>
   );
 };
--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const SearchBar = ()=>{
-    return(
-        <></>
-    )
-}
+export const SearchBar = ({ allData, createUniques, filterData }) => {
+  const [inputValues, setInputValues] = useState({ country: "", industry: "" });
+
+  return (
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          filterData(inputValues.country, inputValues.industry);
+        }}
+      >
+        <label htmlFor="country">Country:</label>
+        <input
+          type="text"
+          id="country"
+          name="country"
+          value={inputValues.country}
+          list="countryList"
+          onChange={(e) =>
+            setInputValues((state) => ({ ...state, country: e.target.value }))
+          }
+        />
+        <datalist id="countryList">
+          {createUniques(allData, "country").map((name) => (
+            <option value={name} key={name} />
+          ))}
+        </datalist>
+        <input
+          type="text"
+          id="industry"
+          name="industry"
+          value={inputValues.industry}
+          list="industryList"
+          onChange={(e) =>
+            setInputValues((state) => ({ ...state, industry: e.target.value }))
+          }
+        />
+        <datalist id="industryList">
+          {createUniques(allData, "industry").map((name) => (
+            <option value={name} key={name} />
+          ))}
+        </datalist>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
