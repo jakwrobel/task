3091080c49074eb6241dbfe5eac69ff0beefe39e
jakwrobel <jak.wrobel96@gmail.com>
2022-01-31 01:14:52 +0100

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {Searchbar} from "./components/searchbar/Searchbar"
 import {Results} from "./components/results/Results"
-import styles from "./styles/styles.scss"
 const App = () => {
   return <div className="App">
     <Searchbar/>
