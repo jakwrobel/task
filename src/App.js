7aa2860933a2c918eb627542ba3eb12297742dab
jakwrobel <jak.wrobel96@gmail.com>
2022-01-31 01:10:17 +0100

    Add components structure and imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react"
+import {Searchbar} from "./components/searchbar/Searchbar"
+import {Results} from "./components/results/Results"
+import styles from "./styles/styles.scss"
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Searchbar/>
+    <Results/>
+  </div>;
 };
 
 export default App;
