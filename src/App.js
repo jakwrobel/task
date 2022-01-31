bdde042b4e09b7311eb8bc9d36e6c8539e5d0ddb
jakwrobel <jak.wrobel96@gmail.com>
2022-01-31 01:44:09 +0100

    Create file with API call and implement it in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react"
-import {Searchbar} from "./components/searchbar/Searchbar"
-import {Results} from "./components/results/Results"
+import React, { useState, useEffect } from "react";
+import { getData } from "./utils/getData";
+import { Searchbar } from "./components/searchbar/Searchbar";
+import { Results } from "./components/results/Results";
 const App = () => {
-  return <div className="App">
-    <Searchbar/>
-    <Results/>
-  </div>;
+  const [companies, setCompanies] = useState();
+
+  useEffect(() => getData().then((res) => setCompanies(res)), []);
+  console.log("stejt:", companies);
+
+  getData();
+  return (
+    <div className="App">
+      <Searchbar />
+      <Results />
+    </div>
+  );
 };
 
 export default App;
